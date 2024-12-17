a7503089caa8fd22df76e294ea9a69c13d6afe3f
Mezyann TELLAL <mtellal@paul-f3Br5s4.clusters.42paris.fr>
2024-12-17 14:54:56 +0100

    cleaning level07 source + walkthrough

---
+++ level07/source.c
@@ -0,0 +1,83 @@
+void clear_stdin() {
+	int var;
+
+	var = getchar();
+	while (var != -1) {
+		if ((char)var == '\n')
+			return 
+		var = getchar(); 
+	}
+}
+
+unsigned int get_unum() {
+	unsigned int num;
+
+	fflush(stdout);
+	scanf("%u", &num);
+	clear_stdin();
+	return num;	
+}
+
+int	read(char *buffer) {
+
+	int	index;
+
+	print(" Index: ");
+	index = get_unum();
+	printf(" Number at data[%u] is %u\n",iVar1,*(undefined4 *)(iVar1 * 4 + param_1));
+	return 0;
+}
+
+int	store_number(char *buffer) {
+
+	unsigned int 	number;
+	unsigned int 	index;
+	int		err;
+
+	printf(" Number: ");
+	number = get_unum();
+	printf(" Index: ");
+	index = get_unum();
+	if ((index % 3 === 0) || ((number >> 24) == 183)) {
+		puts(" *** ERROR! ***");
+		puts("   This index is reserved for wil!");
+		puts(" *** ERROR! ***");
+		ret = 1;
+	}
+	else {
+		*(unsigned int *)(buffer + (index * 4)) = number;
+		err = 0;
+	}
+	return err;
+}
+
+int main(int argc, char **argv) {
+
+	char 	buffer[100];
+	char 	cmd_buff[20];
+	int	ret;
+
+	memset(buffer, 0, 100);
+	memset(cmd_buff, 0, 20);
+	puts("----------------------------------------------------\n  Welcome to wil\'s crappy number stora ge service!   \n----------------------------------------------------\n Commands:                                          \n    store - store a number into the data storage    \n    read  - read a number from the data storage     \n    quit  - exit the program                        \n----------------------------------------------------\n   wil has reserved some storage :>                 \n----------------------------------------------------\n");	
+	while (true) {
+
+		printf("Input command: ");
+		ret = 1;
+		fgets(buff_cmd, 20, stdin);
+
+		if (!strcmp(buff_cmd, "store"))
+			ret = store_number(buffer);
+		else if (!strcmp(buff_cmd, "read"))
+			ret = read_number(buffer);
+		else if (!strcmp(buff_cmd, "quit"))
+			return 0;
+
+		if (ret == 0) 
+			printf(" Completed %s command successfully\n", buff_cmd);
+		else
+			printf(" Failed to do %s command\n", buff_cmd);
+		memset(buff_cmd, 0, 20);	
+	}
+	return 0;
+}
